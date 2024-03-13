7784939c4ca9082acfb018e54f3dbd17558d59d0
Nelly-98 <nelly.guepnang@gmail.com>
2024-03-13 00:07:17 +0000

    we dit it

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !!"}
